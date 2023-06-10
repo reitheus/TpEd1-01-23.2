61b19274a4f563633722c34cdc0725c24f7ea41b
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-10 00:16:36 -0300

    Update Labirinto.c

--- Labirinto.c
+++ Labirinto.c
@@ -73,7 +73,7 @@ Lab* leLabirinto(char *opcao){
     for(int i = 0; i < L ; i++){
        
         //scanf("%c", &(plab->mapa[i][j]));
-        fgets(&(plab->mapa[i]), C ,stdin);
+        fgets( plab->mapa[i] , C ,stdin);
         
 
     }

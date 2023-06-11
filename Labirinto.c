a5e67284db2ee435d1926a323ec14dbeaefd0b2a
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-11 01:37:52 -0300

    Update Labirinto.c

--- Labirinto.c
+++ Labirinto.c
@@ -119,8 +119,80 @@ void posMause(Lab *pLab, Pos *mause){
 //acha a saida do labirinto
 void achaSaida(Lab *pLab, Pos *saida, Pos *mause){
     
+  
+    
+    printf("posição atual L %i C %i \n", valueY(mause), valueX(mause));
+    
+    if(valueY(mause) == valueY(saida) && valueX(mause) == valueX(saida)){
+        printf("achou a saida");
+        printf("coordenadas atuais L %i C %i", valueY(mause), valueX(mause));    
+        
+        return;
+    
+    }
     
     
+    //teste para cima
+    //se estiver dentro do labirinto e se espaço vazio
+    if(valueY(mause) - 1 > 0 && valueY(mause) - 1 <  pLab->tamL && pLab->mapa[valueY(mause) - 1 ][valueX(mause)] == ' '){
+        
+        plab->mapa[valueY(mause)][valueX(mause)] = 'b';
+        
+        updatePos(mause, valueY(mause) - 1, valueX(mause));
+        
+        
+        achaSaida(pLab, saida, mause);
+        
+        updatePos(mause, valueY(mause) + 1, valueX(mause));
+    
+    }
+    
+    
+    //teste para baixo
+    //se estiver dentro do labirinto e se espaço vazio
+    if(valueY(mause) + 1 > 0 && valueY(mause) + 1 <  pLab->tamL && pLab->mapa[valueY(mause) + 1 ][valueX(mause)] == ' '){
+       
+        updatePos(mause, valueY(mause) + 1, valueX(mause));
+        
+        
+        achaSaida(pLab, saida, mause);
+        
+        updatePos(mause, valueY(mause) - 1, valueX(mause));
+    
+    }
+    
+    
+    //teste para esquerda
+    //se estiver dentro do labirinto e se espaço vazio
+    if(valueX(mause) - 1 > 0 && valueX(mause) - 1 <  pLab->tamC && pLab->mapa[valueY(mause)][valueX(mause) - 1] == ' '){
+        
+        updatePos(mause, valueY(mause) , valueX(mause) - 1);
+        
+        achaSaida(pLab, saida, mause);
+        
+        updatePos(mause, valueY(mause) , valueX(mause) + 1);
+    
+    }
+    
+    //teste para direita
+    //se estiver dentro do labirinto e se espaço vazio
+    if(valueX(mause) + 1 > 0 && valueX(mause) + 1 <  pLab->tamC && pLab->mapa[valueY(mause)][valueX(mause) + 1] == ' '){
+        
+        updatePos(mause, valueY(mause) , valueX(mause) + 1);
+        
+        achaSaida(pLab, saida, mause);
+        
+        updatePos(mause, valueY(mause) , valueX(mause) - 1);
+    
+    }
+    
+
+    
+    if( !(valueY(mause) == valueY(saida) && valueX(mause) == valueX(saida)) ){
+        plab->mapa[valueY(mause)][valueX(mause)] = 'x';//atribui x para os locais que o rato já passou e não teve saida
+        return;
+    
+    }
     
     //utilizar recursão
     //teste

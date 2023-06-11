8967e4e8c314368e4a4c4838e8716d8e92dac344
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-10 22:04:24 -0300

    Update Labirinto.h

--- Labirinto.h
+++ Labirinto.h
@@ -15,7 +15,7 @@ void printLab(Lab *plab);
 
 void posMause(Lab *pLab, Pos *mause);//acha a posição do rato
 
-void achaSaida(Lab*);
+void achaSaida(Lab *pLab, Pos *saida, Pos *mause);
 
 void imprimePercursoNoLabirinto(Lab*);
 #endif //Labirinto.h

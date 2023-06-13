020c70318eacf5fe1ca8fadcf681b511bbd167ac
reitheus <75027532+reitheus@users.noreply.github.com>
2023-06-13 20:13:51 -0300

    Add files via upload

---
+++ TpEd/Labirinto.h
@@ -0,0 +1,31 @@
+#ifndef LABIRINTO_H
+#define LABIRINTO_H
+
+#include "percurso.h"
+#include "posicao.h"
+
+typedef struct {
+    char **mapa;
+    char op;
+    int tamL;
+    int tamC;
+    int v;
+}Labirinto;
+
+Labirinto* alocaLab(int, int );
+
+void desalocaLab(Labirinto** );
+int verificavazio(Labirinto* );
+Labirinto* lelabirinto(char );
+
+
+
+void printLab(Labirinto* );
+
+void posMause(Labirinto*, Posicao* );
+
+void achaSaida(Labirinto*, Posicao* , Posicao* , Percurso* , int );
+
+void imprimePercursoNoLabirinto(Labirinto* ,Percurso* );
+
+#endif // LABIRINTO_H

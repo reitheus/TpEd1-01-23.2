238c60bed305e77d90d294590cacaf81c559abed
reitheus <75027532+reitheus@users.noreply.github.com>
2023-06-14 01:09:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,25 +5,23 @@
 
 int main(){
     // int linhas, colunas;
-    char opcao='p';
+    char opcao=' ';
     
      
     Percurso *pTra;
+    Posicao *inicio = alocaPos();
     Posicao *saida = alocaPos();//aloca a posição ára saida
     Posicao *mause = alocaPos();//aloca uma posição para o rato
-    Posicao *inicio;
     Labirinto *pLab;
     pLab = lelabirinto(opcao);
-    inicio = alocaPos();
+
     posMause( pLab, mause);
     updatePos(inicio, valueY(mause), valueX(mause));
     printf("\n");     
-    updatePos( saida, pLab->tamL - 2, pLab->tamC -2);
+    updatePos( saida, pLab->tamL - 2, pLab->tamC -1);
     pTra = alocaPer(pLab->v);
-    printf("\nentrou recursao\n");
     achaSaida(pLab, saida, mause,pTra,1, inicio);
-    printf("\nsaiu recursão\n");
-    pLab=imprimepercursoNolabirinto(pLab,pTra);
+    pLab = imprimepercursoNolabirinto(pLab,pTra);
     
     desalocaPos(&inicio);
     desalocaPos(&mause); 

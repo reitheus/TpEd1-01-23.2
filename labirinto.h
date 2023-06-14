d40abef7d740d6c2ca281072e6b2251fe4746c59
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-14 01:20:13 -0300

    Update labirinto.h

--- labirinto.h
+++ labirinto.h
@@ -24,7 +24,7 @@ void printLab(Labirinto* );
 
 void posMause(Labirinto*, Posicao* );
 
-int achaSaida(Labirinto*, Posicao* , Posicao* , Percurso* ,int ,Posicao* , int achou);
+int achaSaida(Labirinto*, Posicao* , Posicao* , Percurso* ,int ,Posicao* , int );
 
 Labirinto* imprimepercursoNolabirinto(Labirinto* ,Percurso* );
 

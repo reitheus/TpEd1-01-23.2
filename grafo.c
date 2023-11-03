3c8f48ed46c9e173bd5017b141f975e8f8bf26e7
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2023-11-03 12:07:46 -0300

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -4,83 +4,80 @@
 
 //funções da biblioteca 
 
-//função que aloca memória dinamicamente para receber os dados das cidades de acordo com o número de cidades informado 
-int **alocarGrafo(int **cidades, int n){
-	
-	cidades = (int **)malloc(n * sizeof(int *));
+// Definindo a estrutura do grafo
+struct grafo {
+    int **city;
+    int distancia;
+    int *ordemCidades;
+};
+
+// Função para alocar memória para o grafo
+Grafo **alocarGrafo(int n) {
+    Grafo **cidades = (Grafo **)malloc(n * sizeof(Grafo *));
+
     for (int i = 0; i < n; i++) {
-        cidades[i] = (int *)malloc(n * sizeof(int));
+        cidades[i] = (Grafo *)malloc(n * sizeof(Grafo));
     }
-    
-	return cidades; 
+
+    return cidades;
 }
 
-//função libera memória alocada 
-int** desalocarGrafo(int **cidades, int n){
-
-	for (int i = 0; i < n; i++) {
+// Função para liberar memória alocada para o grafo
+Grafo **desalocarGrafo(Grafo **cidades, int n) {
+    for (int i = 0; i < n; i++) {
         free(cidades[i]);
     }
-    free(*cidades);
+    free(cidades);
 
-	return cidades;
-} 
-
-//função para ler os dados de entrada fornecidos pelo usuário 
-int **leGrafo(int **cidades, int n){
-
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++)
-		scanf("%d ", &cidades[i][j]);
-	}
-
-	return cidades; 
+    return NULL;
 }
 
-//função recursiva que retorna o menor caminho
-int encontraCaminho(int **cidade, int n, int menorvalor, int aux, int n2, int distancia) {
-
-	if (n == -1) {
-        return distancia; 
+// Função para ler os dados de entrada fornecidos pelo usuário
+Grafo **leGrafo(Grafo **cidades, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", cidades[i]->city[j]);
+        }
     }
-    
+
+    return cidades;
+}
+
+// Função recursiva para encontrar o menor caminho
+Grafo *encontraCaminho(Grafo **cidades, int n, int menorvalor, int aux, int n2, Grafo *d, Grafo *trajeto) {
+    if (n == -1) {
+        return d;
+    }
+
     if (aux == n) {
-        return encontraCaminho(cidade, n, menorvalor, aux-1, n2, distancia);
+        return encontraCaminho(cidades, n, menorvalor, aux - 1, n2, d, trajeto);
     }
-    
-    if (aux < n) {	
-        if (menorvalor > cidade[n][aux]) {
-            menorvalor = cidade[n][aux];
+
+    if (aux < n) {
+        if (menorvalor > *cidades[n]->city[aux]) {
+            menorvalor = *cidades[n]->city[aux];
         }
-        return encontraCaminho(cidade, n, menorvalor, aux-1, n2, distancia);
+        return encontraCaminho(cidades, n, menorvalor, aux - 1, n2, d, trajeto);
     }
-    
-    if (aux == 0) {	
-        if (menorvalor > cidade[n][aux]) {
-            menorvalor = cidade[n][aux];
+
+    if (aux == 0) {
+        if (menorvalor > *cidades[n]->city[aux]) {
+            menorvalor = *cidades[n]->city[aux];
         }
-        distancia += menorvalor;
-		printf("%d ", n);
+        d->distancia += menorvalor;
         aux = n2;
-		menorvalor = 50000;
-        return encontraCaminho(cidade, n - 1, menorvalor, aux, n2, distancia); 
-    } 
-	return distancia;	
-}
-//imprime menor caminho e a distância percorrida 
-void imprimeCaminho(int **cidades, int n, int distancia){
-	
-	//for(int i = 0; i < n; i++){
-	//	for(int j = 0; j < n; j++){
-	//		printf("%d", i );
-	//	}
-	
-	printf("\n%d", distancia );
+        menorvalor = 50000;
+        return encontraCaminho(cidades, n - 1, menorvalor, aux, n2, d, trajeto);
+    }
+
+    return d;
 }
 
+// Função para imprimir o menor caminho e a distância percorrida
+void imprimeCaminho(int n, Grafo *d, Grafo *trajeto) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", trajeto->ordemCidades[i]);
+    }
 
-
-
-
-
-
+    printf("\n%d", d->distancia);
+}

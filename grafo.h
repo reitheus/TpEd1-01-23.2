152b58b2456bea68dc19c4b3b91a958b536027ae
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2023-11-03 12:05:47 -0300

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -2,16 +2,17 @@
 # define grafo_h
  
 //protótipos das funções 
+typedef struct grafo Grafo;
  
-int** alocarGrafo(int **cidades, int n);
+Grafo** alocarGrafo(int n);
 
-int** desalocarGrafo(int **cidades, int n);
+Grafo** desalocarGrafo(Grafo **cidades, int n);
 
-int** leGrafo(int **cidade, int n);
+Grafo** leGrafo(Grafo **cidade, int n);
 
-int encontraCaminho(int **cidade, int n, int menorvalor, int aux, int n2, int distancia); 
+Grafo* encontraCaminho(Grafo **cidade, int n, int menorvalor, int aux, int n2, Grafo *d, Grafo *trajeto); 
  
-void imprimeCaminho(int **cidades, int n, int distancia); 
+void imprimeCaminho(int n, Grafo *d, Grafo *trajeto); 
 
 # endif
 

49a139ec631e925abc7280320e609aa8812b6a47
reitheus <75027532+reitheus@users.noreply.github.com>
2023-11-09 10:42:43 -0300

    Update grafoponderado.h

--- grafoponderado.h
+++ grafoponderado.h
@@ -2,15 +2,41 @@
 #define GRAFOPONDERADO_H
 
 typedef struct grafo{
-    int **mapa;// Matriz com as distancias
     int *caminho;// vetor do caminho
     int ncity; // numeros de cidade
     int distancia;// distanica do caminho
-
+    Lista *plista;
 }Grafo;
+
+typedef struct item{
+    int *cidades;
+    int cityatual;
+}Item;
+
+typedef struct celula{
+    Item *pItem;
+    Celula *prox;
+}Celula;
+
+typedef struct lista{
+    Celula *pCabeca;
+    Celula *pUltimo;
+}Lista;
 
 //Função de alocação do grafo
 Grafo* alocaGrafo(int );
+
+//Aloca lista
+Lista *alocaLista();
+
+//Desalocacão de lista
+void desalocaLista(Lista** );
+
+//Inicia lista
+void insereLista(Lista* ,Item );
+
+//Remove Lista
+void removeLista(Lista* ,Item );
 
 //Função de desalocação do grafo
 void desalocaGrafo(Grafo** );
@@ -26,5 +52,11 @@ int encontraCaminho(Grafo* ,int ,int ,int );
 
 //Imprime caminho
 void imprimeCaminho(Grafo* );
+
+//Função de ordenação 
+void ordenaLista(Lista* );
+
+//Função imprimi ordenação 
+void imprimeOrdenado(Lista* );
 
 #endif //TAD Grafo Ponderado
